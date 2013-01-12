0089ef5423e4465afdd377aa3e4da592a4c8b4b6
Allan SIMON <allan.simon@supinfo.com>
2013-01-13 04:31:28 +0800

    re #15 continued to write the code for the unit test models::Articles::create_from_lang_and_slug

--- app/tests/models/articles/create_from_lang_and_slug.cpp
+++ app/tests/models/articles/create_from_lang_and_slug.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "models/Articles.h"
+#include "test.h"
 
 #define TEST_NAME "models_create_from_lang_and_slug_test"
 #define DB_SQL_FILE_PATH  "../sql/sqlite3.sql"
@@ -7,7 +8,26 @@
 
 int main() {
 
+
+    bool noTestFailed = true;
     models::Articles articlesModels(TEST_NAME ".db");
-    int result = articlesModels.import_sql_file(DB_SQL_FILE_PATH);
+
+    TEST_RESULT_SHOULD_WORK(
+        "Try load the database ... " ,
+        articlesModels.import_sql_file(DB_SQL_FILE_PATH),
+        noTestFailed
+    );
+    
+    // we should be able to add an article ...
+    TEST_RESULT_SHOULD_WORK(
+        "Try to add an article ... " ,
+        articlesModels.import_sql_file(DB_SQL_FILE_PATH),
+        noTestFailed
+    );
+    
+
+
+
+
 
 }
