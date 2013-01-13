22c1fe608a7facc7a4d1861a3c82509c1c97eff9
Allan SIMON <allan.simon@supinfo.com>
2013-01-13 17:32:27 +0800

    re #15 added check for testing the models::articles::create_from_lang_and_slug

--- app/tests/models/articles/create_from_lang_and_slug.cpp
+++ app/tests/models/articles/create_from_lang_and_slug.cpp
@@ -5,6 +5,7 @@
 #define TEST_NAME "models_create_from_lang_and_slug_test"
 #define DB_SQL_FILE_PATH  "../sql/sqlite3.sql"
 
+#define TEST_ARTICLE_ID  1
 #define TEST_ARTICLE_LANG "fr"
 #define TEST_ARTICLE_SLUG "page_principale"
 #define TEST_ARTICLE_TITLE "Page Principale"
@@ -34,7 +35,7 @@ int main() {
         TEST_ARTICLE_TITLE,
         TEST_ARTICLE_CONTENT
     );
-    if (result == 1) {
+    if (result == TEST_ARTICLE_ID) {
         std::cout << " [ok]" << std::endl;
     } else {
         std::cout << " [fail]" << std::endl;
@@ -58,8 +59,31 @@ int main() {
         noTestFailed = false;
     }
 
-
+    // try to retrieve the article we have added
     
+    
+    // now we check if we can retrieve them
+
+    std::cout << "We now test if we can get it back ... ";
+    results::Article firstArticle = articlesModels.get_from_lang_and_slug(
+        TEST_ARTICLE_LANG,
+        TEST_ARTICLE_SLUG
+    );
+    if ( 
+        (firstArticle.id != TEST_ARTICLE_ID) ||
+        (firstArticle.lang.compare(TEST_ARTICLE_LANG) != 0) ||
+        (firstArticle.slug.compare(TEST_ARTICLE_SLUG) != 0) ||
+        (firstArticle.title.compare(TEST_ARTICLE_TITLE) != 0) ||
+        (firstArticle.content.compare(TEST_ARTICLE_CONTENT) != 0) ||
+        (firstArticle.isLocked != false)
+    ) {
+        noTestFailed = false;
+        std::cout << " [fail]" << std::endl; 
+    
+    } else {
+        std::cout << " [ok]" << std::endl; 
+    }
+ 
     
  
     if (noTestFailed) {
