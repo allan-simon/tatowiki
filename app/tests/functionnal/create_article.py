4e8bdb1ef07f56a9d103610095005ff0d985f614
Allan SIMON <allan.simon@supinfo.com>
2013-04-17 15:15:46 +0800

    close #58, the problem was a missing return, now the bug is closed

--- app/tests/functionnal/create_article.py
+++ app/tests/functionnal/create_article.py
@@ -39,15 +39,26 @@ def create_article(
         post_url = CREATE_ARTICLE_TREAT_URL,
         post_data_array = postData
     )
+    if (not session.test_http_status([302])):
+        print("ERROR we should be redirected");
+        print("status: " + str(session.status))
+        return False
+    if (session.get_redirection() != SHOW_ARTICLE_URL):
+        return False
     return True
 
-
 if __name__ == '__main__':
-    session = Session()
+    error = False
+    session = Session(print_cookies=False)
     login(session)
-    create_article(session)
+    if (not create_article(session)):
+        error = True
+        print("ERROR while trying to add a normal article")
     if (create_article(title = '',slug='prout')):
+        error = True
         print("ERROR we're not supposed to create an article with an empty title")
+    if (not error):
+        print(OK)
 
 
 
