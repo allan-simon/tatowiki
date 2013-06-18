13a415f317adc7312605a6baf6ba1aae07365884
Allan SIMON <allan.simon@supinfo.com>
2013-06-18 22:11:06 +0800

    re #72 added the sql to have a search engine integrated to sqlite3

--- app/sql/sqlite3.sql
+++ app/sql/sqlite3.sql
@@ -85,5 +85,67 @@ begin;
         value text not null 
     );
 
+    -- table used for the search engine
+    create virtual table search using fts4(
+        slug ,
+        title,
+        content,
+        lang,
+        tokenize=porter
+    );
 
-commit ;
+    ---------------------------------------------
+    --
+    -- Triggers to keep the search table sync 
+    -- with the articles tables
+    --
+    ---------------------------------------------
+    
+    -- when we update an article we delete the older one from 
+    -- the search engine
+    create trigger articles_bu before update on articles BEGIN
+        delete from search where docid=old.id;
+    end;
+
+    -- after we've updated, we add the updated article to
+    -- the search engine
+    create trigger articles_au after update on articles begin
+        insert into search(
+            docid,
+            slug,
+            title,
+            content,
+            lang
+        ) values (
+            new.id,
+            new.slug,
+            new.title,
+            new.content,
+            new.lang
+        );
+    end;
+    
+    -- when delete in article, we also delete in the search engine
+    create trigger articles_bd before delete on articles begin
+        delete from search where docid=old.id;
+    end;
+
+
+    -- when we insert, we also insert in the search engine
+    create trigger articles_ai after insert on articles begin
+        insert into search(
+            docid,
+            slug,
+            title,
+            content,
+            lang
+        ) values (
+            new.id,
+            new.slug,
+            new.title,
+            new.content,
+            new.lang
+        );
+    end;
+
+Commit ;
