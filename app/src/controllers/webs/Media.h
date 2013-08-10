e695d9d258504633383b758571e210f0bdae2328
Allan SIMON <allan.simon@supinfo.com>
2013-08-10 13:41:26 +0800

    continued to replace copyright headers by newer

--- app/src/controllers/webs/Media.h
+++ app/src/controllers/webs/Media.h
@@ -1,26 +1,13 @@
 /**
  * Tatoeba wiki  Wiki made with cppcmsskel
+ *
  * Copyright (C) 2012 Allan SIMON <allan.simon@supinfo.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * See accompanying file COPYING.TXT file for licensing details.
  *
  * @category Tatoeba wiki
  * @package  Controllers
  * @author   Allan SIMON <allan.simon@supinfo.com>
- * @license  Affero General Public License
- * @link     https://github.com/sysko/tatowiki@
+ * @link     https://github.com/allan-simon/tatowiki
  */
 
 #ifndef TATOWIKICONTROLLERS_WEBS_MEDIA_H
