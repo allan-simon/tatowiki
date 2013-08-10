c7a10dce0306ff4bc14dbe66024a537c5426571f
Allan SIMON <allan.simon@supinfo.com>
2013-08-10 13:32:56 +0800

    started to replace the old copyright header by a new, shorter, one

--- local_template/generics/Languages.h
+++ local_template/generics/Languages.h
@@ -1,29 +1,14 @@
 /**
  * Tatoeba wiki  Wiki made with cppcmsskel
- * Copyright (C) 2012 Allan SIMON <allan.simon@supinfo.com> 
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * Copyright (C) 2012 Allan SIMON <allan.simon@supinfo.com>
+ * See accompanying file COPYING.TXT file for licensing details.
  *
  * @category Tatoeba wiki
  * @package  Singletons
- * @author   Allan SIMON <allan.simon@supinfo.com> 
- * @license  Affero General Public License
- * @link     https://github.com/sysko/tatowiki@
+ * @author   Allan SIMON <allan.simon@supinfo.com>
+ * @link     https://github.com/allan-simon/tatowiki
  */
-
-
 
 #ifndef CPPCMS_SKEL_GENERICS_LANGUAGES_H
 #define CPPCMS_SKEL_GENERICS_LANGUAGES_H
@@ -42,7 +27,7 @@ typedef std::map<std::string, std::string> InterfaceCodeToName;
 /**
  * Singleton class used to store the information about the
  * languages supported in Tatoeba, and their related info
- * ISO code, name, id in the database 
+ * ISO code, name, id in the database
  */
 class Languages : public Singleton<Languages> {
     friend class Singleton<Languages>;
